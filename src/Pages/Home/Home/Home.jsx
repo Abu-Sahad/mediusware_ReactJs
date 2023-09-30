a2d72f03b5cda7782515359d3e1ef82f4451a526
Abu-Sahad <abusahad603@gmail.com>
2023-09-30 15:54:24 +0600

    Solve problem 1

--- src/Pages/Home/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <div>
-            <h1 className="text-bold text-2xl text-center">This is me .Here is now to the work .please see the task</h1>
-        </div>
-    );
-};
-
-export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Routes.jsx'
+import App from './App'
+//import '../src/assets/scss/App.scss';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
