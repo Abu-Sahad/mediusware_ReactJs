8cbc5f051e77310cacce9f3a10d9a2f44053c284
Abu-Sahad <abusahad603@gmail.com>
2023-09-30 14:12:03 +0600

    project start

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-bold text-2xl text-center">This is me .Here is now to the work .please see the task</h1>
+        </div>
+    );
+};
+
+export default Home;
